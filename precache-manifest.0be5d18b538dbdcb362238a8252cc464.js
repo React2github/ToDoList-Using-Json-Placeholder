2153addc47717a2c6393e05f3ba5ea8ae6d67d8f
Abdella Shukur <51038278+React2github@users.noreply.github.com>
2019-09-05 15:29:56 -0400

    Add files via upload

---
+++ precache-manifest.0be5d18b538dbdcb362238a8252cc464.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670a870cb353e7ce2c22000952ff26db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c7c6e7785698ea7fa9b",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "9346a5a0058751783128",
+    "url": "/static/js/2.cd8769da.chunk.js"
+  },
+  {
+    "revision": "9c7c6e7785698ea7fa9b",
+    "url": "/static/js/main.5f28ad58.chunk.js"
+  },
+  {
+    "revision": "4a85c87eb4000581f2e3",
+    "url": "/static/js/runtime~main.ed07842c.js"
+  }
+]);
